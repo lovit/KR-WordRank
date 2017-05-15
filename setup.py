3637169373f37fac7e2a52a5b85e09de8823a61b
lovit <soy.lovit@gmail.com>
2017-05-15 22:30:30 +0900

    Fix setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from description import __version__, __author__
+from setuptools import setup
+
+readme = 'test'
+
+setup(
+   name="krwordrank",
+   version=__version__,
+   author=__author__,
+   author_email='soy.lovit@gmail.com',
+   url='https://github.com/lovit/KR-WordRank',
+   description="KR-WordRank: Korean Unsupervised Word/Keyword Extractor",
+   long_description=readme,
+   install_requires=["numpy"],
+   packages=['kr_wordrank']
+)
