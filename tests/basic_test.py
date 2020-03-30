e049dffeea2e76b292f122d27f26515854e9a6bb
lovit <soy.lovit@gmail.com>
2020-03-30 19:15:18 +0900

    Add keyword extraction pytest

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,14 +1,19 @@
 import os
+import pytest
 import sys
 root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, root)
+
 import krwordrank
 from krwordrank.hangle import initialize_pattern
 from krwordrank.hangle import normalize
+from krwordrank.word import KRWordRank
 
+
+@pytest.fixture
 def test_config():
     return {
-        'data_path': '{}/data/134963_norm.txt' # La La Land movie comments
+        'data_path': '{}/data/134963_norm.txt'.format(root) # La La Land movie comments
     }
 
 
@@ -27,3 +32,15 @@ def test_normalize():
         assert output_str == expected
         message = form.format(name, input_str, output_str)
         print(message)
+
+
+def test_keyword(test_config):
+    data_path = test_config['data_path']
+    with open(data_path, encoding='utf-8') as f:
+        texts = [line.rsplit('\t')[0].strip() for line in f]
+
+    wordrank_extractor = KRWordRank(min_count = 5, max_length = 10)
+    keywords, rank, graph = wordrank_extractor.extract(texts, beta = 0.85, max_iter = 10)
+    selected_keywords = [word for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:30]]
+    assert selected_keywords[:5] == ['영화', '너무', '정말', '음악', '마지막']
+    print('\nKR-WordRank 라라랜드 영화 리뷰 30 개 키워드\n{}\n'.format(selected_keywords))
