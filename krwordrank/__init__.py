447baf844f3caf2bc21e7f2a1b09d0f475282187
lovit <soy.lovit@gmail.com>
2020-03-30 19:39:12 +0900

    Raise warning if user uses python 2.x

--- krwordrank/__init__.py
+++ krwordrank/__init__.py
@@ -4,6 +4,12 @@ __author__ = 'Lovit'
 __license__ = 'LGPL'
 __copyright__ = 'Copyright 2017 Lovit'
 
+
+import sys
+import warnings
+if sys.version_info.major < 3:
+    warnings.warn('Some functions may not work. We recommend python >= 3.5+')
+
 from . import graph
 from . import hangle
 from . import sentence
