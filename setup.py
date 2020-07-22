86952815df87b67a5e488653d9a4345d4294dbf5
lovit <soy.lovit@gmail.com>
2020-07-23 01:19:27 +0900

    Separate requiremented packages to requirements.txt (Fix #10, #12)

--- setup.py
+++ setup.py
@@ -1,19 +1,35 @@
-import krwordrank
+import os
 from setuptools import setup, find_packages
 
-with open('README.md', encoding='utf-8') as f:
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+
+def get_about():
+    about = {}
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(basedir, 'krwordrank', 'about.py')) as f:
+        exec(f.read(), about)
+    return about
+
+
+def requirements():
+    with open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), encoding='utf-8') as f:
+        return f.read().splitlines()
+
+
+about = get_about()
 setup(
     name="krwordrank",
-    version=krwordrank.__version__,
-    author=krwordrank.__author__,
+    version=about['__version__'],
+    author=about['__author__'],
     author_email='soy.lovit@gmail.com',
     url='https://github.com/lovit/KR-WordRank',
     description="KR-WordRank: Korean Unsupervised Word/Keyword Extractor",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=["numpy>=1.12.1"],
+    install_requires=requirements(),
     keywords = ['Korean word keyword extraction'],
     packages=find_packages()
 )
